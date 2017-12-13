197496e459cf96eea11140abd39fe4278fb97832
Vadim Kononov <vadim.kononov@kaltura.com>
2017-12-13 10:13:44 +0200

    remove Pods folder

--- Example/Pods/Target Support Files/KalturaPlayer/KalturaPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KalturaPlayer : NSObject
-@end
-@implementation PodsDummy_KalturaPlayer
-@end
--- Example/Pods/Target Support Files/Pods-KalturaPlayerExample/Pods-KalturaPlayerExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KalturaPlayerExample : NSObject
-@end
-@implementation PodsDummy_Pods_KalturaPlayerExample
-@end
