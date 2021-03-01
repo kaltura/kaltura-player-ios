ed965cbe2c4af1cc12ba7dfd8b6eca87bddbffc7
Nilit Danan <Nilit.danan@kaltura.com>
2021-03-01 22:40:59 +0200

    Revert "FEC-10389 Add support for referenceId filter in provider (#23) (#26)" (#27)
    This reverts commit 8881d5d6a28b8da345d67954a131d014c7dbbc05.

--- Sources/OVP/OVPMediaOptions.swift
+++ Sources/OVP/OVPMediaOptions.swift
@@ -12,13 +12,11 @@ import PlayKitProviders
     
     @objc public var ks: String?
     @objc public var entryId: String?
-    @objc public var referenceId: String?
     @objc public var uiconfId: NSNumber?
 
     internal func mediaProvider() -> OVPMediaProvider {
         let ovpMediaProvider = OVPMediaProvider()
         ovpMediaProvider.set(entryId: entryId)
-        ovpMediaProvider.set(referenceId: referenceId)
         ovpMediaProvider.set(uiconfId: uiconfId)
         
         return ovpMediaProvider
